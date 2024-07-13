5e86103b6f78d24637cc6b76b164680d2995971c
Prish20 <prish@students.uonbi.ac.ke>
2024-07-14 02:06:23 +0300

    Create the screen

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong Game")
+
+
+screen.exitonclick()
